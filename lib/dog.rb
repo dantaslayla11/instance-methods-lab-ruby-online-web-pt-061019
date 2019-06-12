38eb84ec0e778ee716bc5ed1b0ba0083785e9570
layla dantas <laylatdantas@gmail.com>
2019-06-12 05:04:28 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -9,11 +9,14 @@ end
  
  class Dog
    def sit 
-     puts 
+     puts "The Dog is sitting"
+   end 
+ end
  
 fido = Dog.new
 fido.bark 
-#fido.sit
+fido.sit
  
 snoopy = Dog.new
 snoopy.bark
+snoopy.sit
